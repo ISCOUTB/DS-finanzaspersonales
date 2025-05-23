9e3c37b9d724cc8a5ccab48a08fbe2d68c6f4c3c
Jorge Mario Benavides Angulo <130021357+forever2506@users.noreply.github.com>
2025-05-23 00:26:45 -0500

    Update build.gradle.kts
    gradle.kts actualizado manualmente

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.finanse_tracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" 
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,21 +20,42 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // Especifica tu propio Application ID único
         applicationId = "com.example.finanse_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        release {
+            val keystoreProperties = Properties()
+            val keystorePropertiesFile = rootProject.file("key.properties")
+            if (keystorePropertiesFile.exists()) {
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+            }
+
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Habilita la ofuscación
+            isMinifyEnabled = true
+
+            // Usa ProGuard o R8 para la ofuscación
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), // Archivo predeterminado de ProGuard
+                "proguard-rules.pro" // Archivo personalizado de reglas
+            )
+
+            // Configuración de firma para el tipo release
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
