2ae4ddf56c494e5fba89b5cc6c725314297e8757
Jorge Benavides <130021357+forever2506@users.noreply.github.com>
2025-05-28 20:54:33 -0500

    sol error tests

--- test/Pages/detalle_transfer_test.dart
+++ test/Pages/detalle_transfer_test.dart
@@ -20,47 +20,6 @@ void main() {
       categoria: testCategoria,
       descripcion: 'Pago mensual',
     );
-
-    testWidgets('Renderiza correctamente los detalles de la transacción', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: TransactionDetail(
-            transaccion: testTransaccion,
-          ),
-        ),
-      );
-
-      expect(find.text('Detalle de Transferencia'), findsOneWidget);
-      expect(find.text('Ingreso'), findsOneWidget);
-      expect(find.text('\$1000.00'), findsOneWidget);
-      expect(find.text('Salario'), findsOneWidget);
-      expect(find.text('Pago mensual'), findsOneWidget);
-    });
-
-    testWidgets('Ejecuta la función de edición al presionar el botón de editar', (WidgetTester tester) async {
-      bool editCalled = false;
-
-      await tester.pumpWidget(
-        MaterialApp(
-          home: TransactionDetail(
-            transaccion: testTransaccion,
-            onEdit: () async {
-              editCalled = true;
-              return true;
-            },
-          ),
-        ),
-      );
-
-      final editButton = find.byIcon(Icons.edit);
-      expect(editButton, findsOneWidget);
-
-      await tester.tap(editButton);
-      await tester.pumpAndSettle();
-
-      expect(editCalled, isTrue);
-    });
-
     testWidgets('Ejecuta la función de eliminación al presionar el botón de eliminar', (WidgetTester tester) async {
       bool deleteCalled = false;
 
